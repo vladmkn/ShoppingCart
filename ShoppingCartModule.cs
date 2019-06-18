51ffcf8018ecee218a5a3f7ec3234de2e69e4c9d
vladmk <vladmk@mail.ru>
2019-06-18 21:11:56 +0300

    apiary-mock

--- ShoppingCartModule.cs
+++ ShoppingCartModule.cs
@@ -5,6 +5,28 @@ namespace ShoppingCart
 {
     public class ShoppingCartModule: NancyModule
     {
+        private static string productCatalogBaseUrl = 
+            @"http://private-05cc8-chapter2productcatalogmicroservice.apiary-mock.com";
+
+        private static string getProductPathTemplate =
+            @"/products?productIds=[{0}]";
+
+        private static async Task<HttpResponseMessage> 
+            RequestProductFromProductCatalogue(int[] productCatalogueIds)
+            {
+                var productsResource = string.Format(
+                    getProductPathTemplate, string.Join(",", productCatalogueIds)
+                );
+
+                using(var httpClient = new HttpClient()){
+                    httpClient.BaseAddress = new Uri(productCatalogBaseUrl);
+
+                    return await 
+                        httpClient.GetAsync(productsResource).ConfigureAwait(false);
+                }
+            }
+
+
         public ShoppingCartModule(
             IShoppingCartStore shoppingCartStore,
             IProductCatalogClient productCatalog,
